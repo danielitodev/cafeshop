1c1f8fd6e86d8dffc03820dc89e2b97ebf58be2f
danielitodev <164040429+danielitodev@users.noreply.github.com>
2024-05-09 12:19:43 -0500

    Add files via upload

--- cafishu/listeners.js
+++ cafishu/listeners.js
@@ -1,42 +1,62 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    const homeLink = document.getElementById('home-link');
-  
-    homeLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      location.reload();
-    });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const homeLink = document.getElementById('home-link');
+  homeLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    location.reload();
   });
+});
+  const selectMenuSource = '.select-button';
+  const menuItemClass = 'menu-item';
 
-
-  function selectMenuItem(buttonSelector, menuItemClass) {
-    const selectButton = document.querySelector(buttonSelector);
-    const menuItems = document.querySelectorAll(`.${menuItemClass}`);
-    selectButton.addEventListener('click', () => {
-      menuItems.forEach(item => item.classList.remove('selected'));
-      const menuItem = selectButton.closest(`.${menuItemClass}`);
+  const selectMenuButtons = document.querySelectorAll(selectMenuSource);
+  selectMenuButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const menuItems = document.querySelectorAll(`.${menuItemClass}`);
+      menuItems.forEach((item) => item.classList.remove('selected'));
+      const menuItem = button.closest(`.${menuItemClass}`);
       menuItem.classList.add('selected');
       const selectedBox = menuItem.querySelector('.selected-box');
       selectedBox.style.display = selectedBox.style.display === 'block' ? 'none' : 'block';
     });
-  }
-
-  selectMenuItem('.select-button-1', 'menu-item');
-  selectMenuItem('.select-button-2', 'menu-item');
-  selectMenuItem('.select-button-3', 'menu-item');
-  selectMenuItem('.select-button-4', 'menu-item');
-  selectMenuItem('.select-button-5', 'menu-item');
-  selectMenuItem('.select-button-6', 'menu-item')
+  });
 
   const menuTitle = document.querySelector('.menu-title');
-menuTitle.addEventListener('click', () => {
-  window.location.href = '#menu-opciones';
-});
+  menuTitle.addEventListener('click', () => {
+    window.location.href = '#menu-opciones';
+  });
 
-const contactLink = document.getElementById('contact-link');
-contactLink.addEventListener('click', function(event) {
-  event.preventDefault();
-  const whatsAppNumber = '+573154605970';
-  const url = `https://wa.me/${whatsAppNumber}`;
-  window.open(url, '_blank');
-});
+  const contactLink = document.getElementById('contact-link');
+  contactLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    const whatsAppNumber = '+573154605970';
+    const url = `https://wa.me/${whatsAppNumber}`;
+    window.open(url, '_blank');
+  });
+
+  const selectButtons = document.querySelectorAll('.select-button');
+  selectButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      deselectAllItems();
+      const menuItem = button.closest('.menu-item');
+      menuItem.classList.add('active');
+    });
+  });
+
+  function deselectAllItems() {
+    const menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach((item) => {
+      item.classList.remove('active');
+    });
+  }
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const menuBtn = document.getElementById('menu-btn');
+    const menuItems = document.getElementById('menu-items');
+  
+    menuBtn.addEventListener('click', () => {
+      menuItems.classList.toggle('menu-active');
+    });
+  });
